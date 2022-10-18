777cbc6fc832e02d6c3349b95eb6ac124eddb3ec
Mark Sedrak <mark.sedrak@digio.com.au>
2022-10-18 11:28:53 +1100

    chore: connect to db client, to note delete db afterwards

---
+++ api/postMessage/config.js
@@ -0,0 +1,91 @@
+var config = {}
+
+config.endpoint = 'https://azure-core-compare-mongo.documents.azure.com:443/'
+config.key = '0dksNDFy61MNZlaeOo2x7w8MHD7PPLqg2ASn4d9usQRmoDmxqIfaB3haTdZnTG8oBjRyBsuPyvIZGIDKsOlKnw=='
+
+config.database = {
+  id: 'ToDoList'
+}
+
+config.container = {
+  id: 'Items'
+}
+
+config.items = {
+  Andersen: {
+    id: 'Anderson.1',
+    Country: 'USA',
+    partitionKey: 'USA',
+    lastName: 'Andersen',
+    parents: [
+      {
+        firstName: 'Thomas'
+      },
+      {
+        firstName: 'Mary Kay'
+      }
+    ],
+    children: [
+      {
+        firstName: 'Henriette Thaulow',
+        gender: 'female',
+        grade: 5,
+        pets: [
+          {
+            givenName: 'Fluffy'
+          }
+        ]
+      }
+    ],
+    address: {
+      state: 'WA',
+      county: 'King',
+      city: 'Seattle'
+    }
+  },
+  Wakefield: {
+    id: 'Wakefield.7',
+    partitionKey: 'Italy',
+    Country: 'Italy',
+    parents: [
+      {
+        familyName: 'Wakefield',
+        firstName: 'Robin'
+      },
+      {
+        familyName: 'Miller',
+        firstName: 'Ben'
+      }
+    ],
+    children: [
+      {
+        familyName: 'Merriam',
+        firstName: 'Jesse',
+        gender: 'female',
+        grade: 8,
+        pets: [
+          {
+            givenName: 'Goofy'
+          },
+          {
+            givenName: 'Shadow'
+          }
+        ]
+      },
+      {
+        familyName: 'Miller',
+        firstName: 'Lisa',
+        gender: 'female',
+        grade: 1
+      }
+    ],
+    address: {
+      state: 'NY',
+      county: 'Manhattan',
+      city: 'NY'
+    },
+    isRegistered: false
+  }
+}
+
+module.exports = config
--- api/postMessage/index.js
+++ api/postMessage/index.js
@@ -1,7 +1,19 @@
+const CosmosClient = require('@azure/cosmos').CosmosClient
+
+const config = require('./config')
+const url = require('url')
+
+const endpoint = config.endpoint
+const key = config.key
+
+const databaseId = config.database.id
+const containerId = config.container.id
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
+    const bane = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
@@ -14,8 +26,41 @@ module.exports = async function (context, req) {
         });
     }
 
+    const reponses = [responseMessage];
+    if (bane) {
+        const dbClinet = await getDbClient();
+
+        dbClinet.items.upsert({
+            id: new Date().toISOString() + Math.random().toString().substring(2, 10),
+            name: bane
+        })
+
+
+        reponses.push["did the db insert!"];
+    }
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: reponses.join(" ### ")
     };
+}
+
+async function getDbClient() {
+    const cosmosClient = new CosmosClient({
+        endpoint: process.env.COSMOS_ENDPOINT,
+        key: process.env.COSMOS_KEY,
+    })
+    const database = (
+        await this.cosmosClient.databases.createIfNotExists({
+            id: process.env.COSMOS_ENV, //TODO:: This should be eventually a variable based on the environment.
+        })
+    ).database
+
+    const container = (
+        await this.database.containers.createIfNotExists({
+            id: tableName,
+        })
+    ).container
+
+    return container
 }
